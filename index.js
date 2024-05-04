b852bf00be621de59d571ec788144f7f92654692
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-05-03 20:46:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ let icon =  document.querySelectorAll(".icon")
 
     for (let i = 0; i < button.length; i++) {
         button[i].addEventListener("click", function() {
-            this.classList.toggle("active");
             let content = this.nextElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
                 icon[i].src = "images/icon-plus.svg"
-                
             } else {
                 content.style.display = "block";
                 icon[i].src = "images/icon-minus.svg"
